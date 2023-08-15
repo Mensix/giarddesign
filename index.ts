b1fd2742c4834c91d3a5a6b6e91021410b375c9e
Grzegorz02@ <grzegorz.perun@gmail.com>
2023-08-15 19:53:03 +0200

    feat: drop gsap

--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-import gsap from 'gsap'
-
 const dropdownButton = document.querySelector('#offer-dropdown-button') as HTMLButtonElement
 const dropdown = document.querySelector('#offer-dropdown') as HTMLUListElement
 
@@ -23,68 +21,29 @@ searchInput.addEventListener('input', () => {
   }
 })
 
-const photoPrevious = document.querySelector('#photo-previous') as HTMLButtonElement
 const photoNext = document.querySelector('#photo-next') as HTMLButtonElement
-const photos = [
-  document.querySelector('#photo-0') as HTMLImageElement,
-  document.querySelector('#photo-1') as HTMLImageElement,
-  document.querySelector('#photo-2') as HTMLImageElement,
-]
-let photoOrder = [0, 1, 2]
-
-function animatePhotos(index0: number, index1: number, index2: number, reverse = false) {
-  const tl = gsap.timeline()
-
-  if (reverse) {
-    tl.fromTo(photos[index0], { left: '-100%', zIndex: -3 }, { zIndex: -3, left: '100%', duration: 0 })
-    tl.fromTo(photos[index1], { left: '0' }, { left: '-100%', duration: 0, zIndex: -1 })
-    tl.fromTo(photos[index2], { left: '100%', zIndex: -2 }, { left: '0', duration: 0, zIndex: -1 })
-  }
-  else {
-    tl.fromTo(photos[index0], { left: '-100%', zIndex: -2 }, { left: '0', duration: 0, zIndex: -2 })
-    tl.fromTo(photos[index1], { left: '0' }, { left: '100%', duration: 0, zIndex: -1 })
-    tl.fromTo(photos[index2], { left: '100%', zIndex: -3 }, { left: '-100%', duration: 0, zIndex: -3 })
-  }
-}
-
-photoPrevious.addEventListener('click', () => {
-  // Start: 0 1 2
-  if (photoOrder[0] === 0 && photoOrder[1] === 1 && photoOrder[2] === 2) {
-    /* 1 2 0 */
-    animatePhotos(0, 1, 2, true)
-    // photoIndex = 0
-    photoOrder = [1, 2, 0]
-  }
-  else if (photoOrder[0] === 1 && photoOrder[1] === 2 && photoOrder[2] === 0) {
-    /* 2 0 1 */
-    animatePhotos(1, 2, 0, true)
-    // photoIndex = 1
-    photoOrder = [2, 0, 1]
-  }
-  else if (photoOrder[0] === 2 && photoOrder[1] === 0 && photoOrder[2] === 1) {
-    /* 0 1 2 */
-    animatePhotos(2, 0, 1, true)
-    photoOrder = [0, 1, 2]
-  }
+const photos = document.querySelectorAll('#photo-0, #photo-1, #photo-2') as NodeListOf<HTMLImageElement>
+photos.forEach((photo, index) => {
+  photo.style.transform = `translateX(${index * 100}%)`
+  if (index > 0)
+    photo.style.zIndex = '-1'
 })
 
+let currentPhoto = 0
 photoNext.addEventListener('click', () => {
-  // Start: 0 1 2
-  if (photoOrder[0] === 0 && photoOrder[1] === 1 && photoOrder[2] === 2) {
-    /* 2 0 1 */
-    animatePhotos(0, 1, 2)
-    photoOrder = [2, 0, 1]
-  }
-  else if (photoOrder[0] === 2 && photoOrder[1] === 0 && photoOrder[2] === 1) {
-    /* 1 2 0 */
-    animatePhotos(2, 0, 1)
-    photoOrder = [1, 2, 0]
-  }
-  else if (photoOrder[0] === 1 && photoOrder[1] === 2 && photoOrder[2] === 0) {
-    /* 0 1 2 */
-    animatePhotos(1, 2, 0)
-    photoOrder = [0, 1, 2]
-  }
+  if (currentPhoto === photos.length - 1)
+    currentPhoto = 0
+  else
+    currentPhoto++
+
+  photos.forEach((photo, index) => {
+    photo.style.transform = `translateX(${(index - currentPhoto) * 100}%)`
+    if (index === currentPhoto)
+      photo.style.zIndex = '1'
+    else
+      photo.style.zIndex = '-1'
+  })
+  currentPhoto = currentPhoto % photos.length
 })
 
 window.onclick = (event) => {
