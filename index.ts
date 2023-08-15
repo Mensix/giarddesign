58f519405626849af8561313e675f04a42ffde8c
Grzegorz02@ <grzegorz.perun@gmail.com>
2023-08-15 17:53:10 +0200

    feat: add gsap

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import gsap from 'gsap'
+
 const dropdownButton = document.querySelector('#offer-dropdown-button') as HTMLButtonElement
 const dropdown = document.querySelector('#offer-dropdown') as HTMLUListElement
 
@@ -21,43 +23,67 @@ searchInput.addEventListener('input', () => {
   }
 })
 
+const photoPrevious = document.querySelector('#photo-previous') as HTMLButtonElement
 const photoNext = document.querySelector('#photo-next') as HTMLButtonElement
 const photos = [
   document.querySelector('#photo-0') as HTMLImageElement,
   document.querySelector('#photo-1') as HTMLImageElement,
   document.querySelector('#photo-2') as HTMLImageElement,
 ]
-let photoIndex = 1
+let photoOrder = [0, 1, 2]
+
+function animatePhotos(index0: number, index1: number, index2: number, reverse = false) {
+  const tl = gsap.timeline()
+
+  if (reverse) {
+    tl.fromTo(photos[index0], { left: '-100%', zIndex: -3 }, { zIndex: -3, left: '100%', duration: 0 })
+    tl.fromTo(photos[index1], { left: '0' }, { left: '-100%', duration: 0, zIndex: -1 })
+    tl.fromTo(photos[index2], { left: '100%', zIndex: -2 }, { left: '0', duration: 0, zIndex: -1 })
+  }
+  else {
+    tl.fromTo(photos[index0], { left: '-100%', zIndex: -2 }, { left: '0', duration: 0, zIndex: -2 })
+    tl.fromTo(photos[index1], { left: '0' }, { left: '100%', duration: 0, zIndex: -1 })
+    tl.fromTo(photos[index2], { left: '100%', zIndex: -3 }, { left: '-100%', duration: 0, zIndex: -3 })
+  }
+}
+
+photoPrevious.addEventListener('click', () => {
+  // Start: 0 1 2
+  if (photoOrder[0] === 0 && photoOrder[1] === 1 && photoOrder[2] === 2) {
+    /* 1 2 0 */
+    animatePhotos(0, 1, 2, true)
+    // photoIndex = 0
+    photoOrder = [1, 2, 0]
+  }
+  else if (photoOrder[0] === 1 && photoOrder[1] === 2 && photoOrder[2] === 0) {
+    /* 2 0 1 */
+    animatePhotos(1, 2, 0, true)
+    // photoIndex = 1
+    photoOrder = [2, 0, 1]
+  }
+  else if (photoOrder[0] === 2 && photoOrder[1] === 0 && photoOrder[2] === 1) {
+    /* 0 1 2 */
+    animatePhotos(2, 0, 1, true)
+    photoOrder = [0, 1, 2]
+  }
+})
 
 photoNext.addEventListener('click', () => {
-  if (photoIndex === 1) {
+  // Start: 0 1 2
+  if (photoOrder[0] === 0 && photoOrder[1] === 1 && photoOrder[2] === 2) {
     /* 2 0 1 */
-    photos[0].classList.add('left-0')
-    photos[0].classList.remove('-left-full', 'top-0', 'z-[-1]')
-    photos[1].classList.add('absolute', 'left-full', 'top-0', 'z-[-1]')
-    photos[2].classList.remove('left-full')
-    photos[2].classList.add('absolute', '-left-full', 'top-0', 'z-[-1]')
-    photoIndex = 2
+    animatePhotos(0, 1, 2)
+    photoOrder = [2, 0, 1]
   }
-  else if (photoIndex === 2) {
+  else if (photoOrder[0] === 2 && photoOrder[1] === 0 && photoOrder[2] === 1) {
     /* 1 2 0 */
-    photos[0].classList.add('absolute', 'left-full', 'z-[-1]')
-    photos[0].classList.remove('-left-full', 'top-0')
-    photos[1].classList.remove('left-full')
-    photos[1].classList.add('absolute', '-left-full', 'top-0', 'z-[-1]')
-    photos[2].classList.add('left-0')
-    photos[2].classList.remove('-left-full', 'left-full', 'top-0', 'z-[-1]')
-    photoIndex = 0
+    animatePhotos(2, 0, 1)
+    photoOrder = [1, 2, 0]
   }
-  else if (photoIndex === 0) {
+  else if (photoOrder[0] === 1 && photoOrder[1] === 2 && photoOrder[2] === 0) {
     /* 0 1 2 */
-    photos[0].classList.remove('left-full')
-    photos[0].classList.add('absolute', '-left-full', 'top-0', 'z-[-1]')
-    photos[1].classList.add('left-0')
-    photos[1].classList.remove('-left-full', 'top-0', 'z-[-1]')
-    photos[2].classList.add('absolute', 'left-full', 'z-[-1]', 'top-0')
-    photos[2].classList.remove('-left-full')
-    photoIndex = 1
+    animatePhotos(1, 2, 0)
+    photoOrder = [0, 1, 2]
   }
 })
 
