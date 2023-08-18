649f1b48eafdb16ba55cb32669ad3abd086b9b5d
Grzegorz02@ <grzegorz.perun@gmail.com>
2023-08-18 19:28:19 +0200

    feat: dialog

--- slider.ts
+++ slider.ts
@@ -9,37 +9,35 @@ function handlePhotoSlider(photos: NodeListOf<HTMLImageElement>, currentPhoto: n
   return currentPhoto
 }
 
-const photos = document.querySelectorAll('#photo-0, #photo-1, #photo-2') as NodeListOf<HTMLImageElement>
 let currentPhoto = 0
+const photosDialog = document.querySelector('#photos-dialog') as HTMLDialogElement
+const photosMasonry = document.querySelectorAll('#photo-4-clickable, #photo-5-clickable, #photo-6-clickable, #photo-7-clickable, #photo-8-clickable, #photo-9-clickable, #photo-10-clickable, #photo-11-clickable, #photo-12-clickable') as NodeListOf<HTMLImageElement>
+const photos = document.querySelectorAll('#photo-4, #photo-5, #photo-6, #photo-7, #photo-8, #photo-9, #photo-10, #photo-11, #photo-12') as NodeListOf<HTMLImageElement>
 
-photos.forEach((photo, index) => {
-  photo.style.transform = `translateX(${index * 100}%)`
+photosMasonry.forEach((photo) => {
+  photo.addEventListener('click', () => {
+    currentPhoto = Number.parseInt(photo.id.split('-')[1]) - 4
+    photosDialog.showModal()
+    document.body.style.overflow = 'hidden'
+
+    photos.forEach((photo, index) => {
+      photo.style.transform = `translateX(${(index - currentPhoto) * 100}%)`
+      if (index - currentPhoto === 0)
+        photo.style.visibility = 'visible'
+    })
+  })
 })
 
-const photoPrevious = document.querySelector('#photo-previous') as HTMLButtonElement
+// photos.forEach((photo, index) => {
+//   photo.style.transform = `translateX(${index * 100}%)`
+// })
+
+const photoPrevious = document.querySelector('#photo-previous2') as HTMLButtonElement
 photoPrevious.addEventListener('click', () => {
   currentPhoto = handlePhotoSlider(photos, currentPhoto, false)
 })
 
-const photoNext = document.querySelector('#photo-next') as HTMLButtonElement
+const photoNext = document.querySelector('#photo-next2') as HTMLButtonElement
 photoNext.addEventListener('click', () => {
   currentPhoto = handlePhotoSlider(photos, currentPhoto, true)
-})
-
-const photosDialog = document.querySelector('#photos-dialog') as HTMLDialogElement
-const photos2 = document.querySelectorAll('#photo-100, #photo-101, #photo-102, #photo-103, #photo-104, #photo-105, #photo-106, #photo-107, #photo-108') as NodeListOf<HTMLImageElement>
-let currentPhoto2 = 0
-
-photos2.forEach((photo, index) => {
-  photo.style.transform = `translateX(${index * 100}%)`
-})
-
-const photoPrevious2 = document.querySelector('#photo-previous2') as HTMLButtonElement
-photoPrevious2.addEventListener('click', () => {
-  currentPhoto2 = handlePhotoSlider(photos2, currentPhoto2, false)
-})
-
-const photoNext2 = document.querySelector('#photo-next2') as HTMLButtonElement
-photoNext2.addEventListener('click', () => {
-  currentPhoto2 = handlePhotoSlider(photos2, currentPhoto2, true)
 })
