51dcc4c3e6757b073894e120ed32c96444ac7b1d
Grzegorz02@ <grzegorz.perun@gmail.com>
2023-08-19 20:32:53 +0200

    feat: small touches

--- masonry.ts
+++ masonry.ts
@@ -1,7 +1,6 @@
 import handlePhotoSlider from './slider'
 
 const photosDialog = document.querySelector<HTMLDialogElement>('#photos-dialog')!
-const photosClose = document.querySelector<HTMLButtonElement>('#photos-close')!
 const photosMasonry = document.querySelectorAll<HTMLImageElement>('#photo-4-clickable, #photo-5-clickable, #photo-6-clickable, #photo-7-clickable, #photo-8-clickable, #photo-9-clickable, #photo-10-clickable, #photo-11-clickable, #photo-12-clickable')
 const photos = document.querySelectorAll<HTMLImageElement>('#photo-4, #photo-5, #photo-6, #photo-7, #photo-8, #photo-9, #photo-10, #photo-11, #photo-12')
 
@@ -27,11 +26,6 @@ function handlePhotoNext() {
   currentPhoto = handlePhotoSlider(photos, currentPhoto, true)
 }
 
-function closePhotoDialog() {
-  photosDialog.close()
-  document.body.style.overflow = ''
-}
-
 photosMasonry.forEach((photo) => {
   photo.addEventListener('click', () => {
     showPhotoDialog(photo)
@@ -43,5 +37,3 @@ photoPrevious.addEventListener('click', handlePhotoPrevious)
 
 const photoNext = document.querySelector<HTMLButtonElement>('#photo-next2')!
 photoNext.addEventListener('click', handlePhotoNext)
-
-photosClose.addEventListener('click', closePhotoDialog)
