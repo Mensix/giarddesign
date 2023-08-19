774d3a53bc6d7bed8b555d08a348a149a203cde8
Grzegorz02@ <grzegorz.perun@gmail.com>
2023-08-19 10:23:21 +0200

    feat: finish site

--- reveal.ts
+++ reveal.ts
@@ -7,15 +7,15 @@ function toggleExpanded() {
   const photoElements = document.querySelectorAll<HTMLElement>('#photo-8-clickable, #photo-9-clickable, #photo-10-clickable, #photo-11-clickable, #photo-12-clickable')
   const showMoreText = isExpanded ? 'Rozwiń' : 'Zwiń'
   const showMoreSvgPath = isExpanded ? 'M19.5 8.25l-7.5 7.5-7.5-7.5' : 'M4.5 15.75l7.5-7.5 7.5 7.5'
-  const overlayClassesToRemove = isExpanded ? ['bg-gradient-to-b', 'from-transparent', 'to-[#DCC1AB]', 'z-[1]'] : ['z-0']
+  const overlayClassesToRemove = isExpanded ? ['bg-gradient-to-b', 'from-transparent', 'to-[#DCC1AB]', 'z-0'] : ['z-[1]']
   const overlayClassesToAdd = isExpanded ? ['z-0'] : ['bg-gradient-to-b', 'from-transparent', 'to-[#DCC1AB]', 'z-[1]']
   const photoClassesToAdd = isExpanded ? ['hidden'] : ['flex']
   const photoClassesToRemove = isExpanded ? ['flex'] : ['hidden']
   const paddingBottom = isExpanded ? '' : '8rem'
 
   photoElements.forEach((element) => {
-    element.classList.remove(...photoClassesToAdd)
-    element.classList.add(...photoClassesToRemove)
+    element.classList.remove(...photoClassesToRemove)
+    element.classList.add(...photoClassesToAdd)
   })
 
   showMore.style.bottom = isExpanded ? '' : '3rem'
