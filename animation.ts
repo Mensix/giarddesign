1d90a59e6c279cffbe90b70a1bab221ad0a7a0c4
Grzegorz02@ <grzegorz.perun@gmail.com>
2023-08-19 09:05:40 +0200

    feat: add animations

---
+++ animation.ts
@@ -0,0 +1,28 @@
+window.addEventListener('scroll', () => {
+//   const reveals = document.querySelectorAll('#reveal')
+//   for (let i = 0; i < reveals.length; i++) {
+//     const windowHeight = window.innerHeight
+//     const elementTop = reveals[i].getBoundingClientRect().top
+//     const elementVisible = 100
+//     if (elementTop < windowHeight - elementVisible)
+//       reveals[i].classList.add('animate__animated', 'animate__fadeInUp')
+  //   }
+
+  // prevent multiple animation
+  const reveals = document.querySelectorAll('#reveal')
+  const wasFired = []
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight
+    const elementTop = reveals[i].getBoundingClientRect().top
+    const elementVisible = 25
+    if (elementTop < windowHeight - elementVisible) {
+      if (!wasFired[i]) {
+        reveals[i].classList.add('animate__animated', 'animate__fadeInUp')
+        wasFired[i] = true
+      }
+    }
+    else {
+      wasFired[i] = false
+    }
+  }
+})
